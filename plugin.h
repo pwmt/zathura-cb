243072127f89532344009c49486cfef6dfd4f02c
Sebastian Ramacher <sebastian+dev@ramacher.at>
2017-01-26 22:34:16 +0100

    Hide symbols

--- plugin.h
+++ plugin.h
@@ -9,6 +9,8 @@
 #include <cairo.h>
 #endif
 
+#include <girara/macros.h>
+
 #include <zathura/plugin-api.h>
 
 typedef struct cb_document_s cb_document_t;
@@ -20,7 +22,7 @@ typedef struct cb_page_s cb_page_t;
  * @param document The document
  * @return ZATHURA_ERROR_OK if no error occurred
  */
-zathura_error_t cb_document_open(zathura_document_t* document);
+GIRARA_HIDDEN zathura_error_t cb_document_open(zathura_document_t* document);
 
 /**
  * Frees the document
@@ -29,7 +31,7 @@ zathura_error_t cb_document_open(zathura_document_t* document);
  * @param data Custom data
  * @return ZATHURA_ERROR_OK if no error occurred
  */
-zathura_error_t cb_document_free(zathura_document_t* document, cb_document_t* cb_document);
+GIRARA_HIDDEN zathura_error_t cb_document_free(zathura_document_t* document, cb_document_t* cb_document);
 
 /**
  * Generates the index of the document
@@ -40,7 +42,7 @@ zathura_error_t cb_document_free(zathura_document_t* document, cb_document_t* cb
  * @return Tree node object or NULL if an error occurred (e.g.: the document has
  *   no index)
  */
-girara_tree_node_t* cb_document_index_generate(zathura_document_t* document,
+GIRARA_HIDDEN girara_tree_node_t* cb_document_index_generate(zathura_document_t* document,
     cb_document_t* cb_document, zathura_error_t* error);
 
 /**
@@ -58,7 +60,7 @@ zathura_error_t cb_page_init(zathura_page_t* page);
  * @param cb_page cb Page
  * @return ZATHURA_ERROR_OK if no error occurred
  */
-zathura_error_t cb_page_clear(zathura_page_t* page, cb_page_t* cb_page);
+GIRARA_HIDDEN zathura_error_t cb_page_clear(zathura_page_t* page, cb_page_t* cb_page);
 
 /**
  * Renders the page to a cairo object
@@ -69,7 +71,7 @@ zathura_error_t cb_page_clear(zathura_page_t* page, cb_page_t* cb_page);
  * @param printing Render for printing
  * @return ZATHURA_ERROR_OK if no error occurred
  */
-zathura_error_t cb_page_render_cairo(zathura_page_t* page, cb_page_t* cb_page,
+GIRARA_HIDDEN zathura_error_t cb_page_render_cairo(zathura_page_t* page, cb_page_t* cb_page,
     cairo_t* cairo, bool printing);
 
 #endif // CB_H
