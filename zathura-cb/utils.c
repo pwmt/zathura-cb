cdb306d2aaaab0825f3bcddef3863cd708fe6c95
Sebastian Ramacher <sebastian@ramacher.at>
2022-05-24 23:58:52 +0200

    Fix ordering of file names (fixes #6)

--- zathura-cb/utils.c
+++ zathura-cb/utils.c
@@ -9,9 +9,15 @@ compare_path(const char* str1, const char* str2)
 {
   char* ustr1 = g_utf8_casefold(str1, -1);
   char* ustr2 = g_utf8_casefold(str2, -1);
-  int result  = g_utf8_collate(ustr1, ustr2);
-  g_free(ustr1);
+
+  char* ustr3 = g_utf8_collate_key_for_filename(ustr1, -1);
+  char* ustr4 = g_utf8_collate_key_for_filename(ustr2, -1);
   g_free(ustr2);
+  g_free(ustr1);
+
+  int result  = g_strcmp0(ustr3, ustr4);
+  g_free(ustr4);
+  g_free(ustr3);
 
   return result;
 }
