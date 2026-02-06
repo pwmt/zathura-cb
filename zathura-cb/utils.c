10cba60bae1438e9334a3ac39c9925594ebcb886
Sebastian Ramacher <sebastian@ramacher.at>
2026-02-06 23:52:52 +0100

    Use automatic clean up

--- zathura-cb/utils.c
+++ zathura-cb/utils.c
@@ -5,17 +5,11 @@
 #include "utils.h"
 
 int compare_path(const char* str1, const char* str2) {
-  char* ustr1 = g_utf8_casefold(str1, -1);
-  char* ustr2 = g_utf8_casefold(str2, -1);
+  g_autofree char* ustr1 = g_utf8_casefold(str1, -1);
+  g_autofree char* ustr2 = g_utf8_casefold(str2, -1);
 
-  char* ustr3 = g_utf8_collate_key_for_filename(ustr1, -1);
-  char* ustr4 = g_utf8_collate_key_for_filename(ustr2, -1);
-  g_free(ustr2);
-  g_free(ustr1);
+  g_autofree char* ustr3 = g_utf8_collate_key_for_filename(ustr1, -1);
+  g_autofree char* ustr4 = g_utf8_collate_key_for_filename(ustr2, -1);
 
-  int result = g_strcmp0(ustr3, ustr4);
-  g_free(ustr4);
-  g_free(ustr3);
-
-  return result;
+  return g_strcmp0(ustr3, ustr4);
 }
